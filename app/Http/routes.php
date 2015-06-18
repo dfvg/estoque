11f5204aba68d1bf62697adf1d7c1884c9bc5d8f
Diego Giuliani <diego.giuliani@gmail.com>
2015-06-18 13:14:38 -0300

    Iniciando as primeiras rotas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function() {
+    return '<h1>Primeira lógica com Laravel</h1>';
+});
+
+Route::get('/outra', function() {
+    return '<h1>Outra lógica com Laravel</h1>';
+});
